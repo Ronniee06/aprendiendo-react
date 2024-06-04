a831460d7cb8a0edb4a98e9e9e15479bb3bd74b6
Ronniee06 <yumacro06@gmail.com>
2024-06-04 22:53:57 +0200

    04-testing

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -24,11 +24,11 @@ export function App() {
             <button onClick={handleClick}>Get new Fact</button>
             <section>
                 {fact && <p>{fact}</p>}
-                {imageUrl && <img src={imageUrl} alt="" />}
-                <Otro fact={fact}/>
-                <Otro fact={fact}/>
-                <Otro fact={fact}/>
+                {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+        
             </section>
         </main>
     )
+
+    //<!--<Otro fact={fact}/>-->
 }
